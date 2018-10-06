f7e4fa2032775b760f8de4d12a630ea23ddfa536
Daniel Carbajal <danielcarbajalescamillac@gmail.com>
2018-10-05 22:52:31 -0600

    se agrego validacion para declaraciones de variables duplicadas.

--- src/simbolos/TablaDeSimbolos.java
+++ src/simbolos/TablaDeSimbolos.java
@@ -8,7 +8,15 @@ public class TablaDeSimbolos {
     HashMap<String,Identificador> simbolos = new HashMap();
 
     public void agregarIdentificador(Identificador identificador){
-        simbolos.put(identificador.getToken().image,identificador);
+        if (!simbolos.containsKey(identificador.getToken().image)) {
+            simbolos.put(identificador.getToken().image, identificador);
+        } else {
+            String reason = "La variable \""+identificador.getToken().image+"\" ya esta declarada";
+            throw new SemanticError(
+                    identificador.getToken().beginLine,
+                    identificador.getToken().beginColumn,
+                    reason);
+        }
     }
 
     public String toString(){
